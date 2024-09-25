bda077b45aa0ba9de468d35672754a905baf7bca
unknown <suraj.sakhare5869@gmail.com>
2024-09-25 18:48:32 +0530

    First Commit

---
+++ ArmstrongNumber/Models/Armstrong.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArmstrongNumber.Models
+{
+    internal class Armstrong
+    {
+        static int divisor = 10;
+        public static bool IsArmstrong(int number)
+        {
+            double answer = 0;
+            int numberOfDigits = CalculateDigits(number);
+            int temp = number;
+
+            //Calculate te power of each digit 
+            while (number > 0)
+            {
+                int digit = number % divisor;
+                number = number / divisor;
+                answer = answer + Math.Pow(digit, numberOfDigits);
+            }
+
+            //Check for armstrong number
+            if (answer == temp)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        //Calculate the number of digits
+        static int CalculateDigits(int number)
+        {
+            int count = 0;
+
+            while (number > 0) { 
+                number = number / divisor;
+                count++;
+            }
+            return count;
+
+        }
+    }
+}
---
+++ ArmstrongNumber/Program.cs
@@ -0,0 +1,27 @@
+﻿using ArmstrongNumber.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArmstrongNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a Number: ");
+            int number = int.Parse(Console.ReadLine());
+
+            if (Armstrong.IsArmstrong(number))
+            {
+                Console.WriteLine($"The number {number} is Armstrong Number.");
+            }
+            else 
+            {
+                Console.WriteLine($"The number {number} is not an Armstrong Number");
+            }
+        }
+    }
+}
